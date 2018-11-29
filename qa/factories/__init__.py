b05d15571c7e6662d37b8da7e976dd9f87ee86be
Abdelrahmen Ayman <abdo_elbakary@outlook.com>
2018-11-29 23:44:47 +0200

    questions and answers app structure and models defenition

---
+++ qa/factories/__init__.py
@@ -0,0 +1,3 @@
+from .course_factory import CourseFactory
+from .question_factory import QuestionFactory
+from .answer_factory import AnswerFactory
---
+++ qa/models/question.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from ..models import Course
+from users.models import Gucian
+
+
+class Question(models.Model):
+    """
+    Represents a question that can be asked on a specific course or topic.
+    """
+    course = models.ForeignKey(
+        Course,
+        related_name='questions',
+        null=True,
+        on_delete=models.SET_NULL)
+    asker = models.ForeignKey(
+        Gucian,
+        related_name='questions',
+        null=True,
+        on_delete=models.SET_NULL)
+
+    title = models.CharField(max_length=150)
+    text = models.CharField(max_length=30000)
+    up_votes = models.IntegerField()
+    down_votes = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def votes(self):
+        return self.up_votes - self.down_votes
+
+    def up_vote(self):
+        self.up_votes += 1
+        self.save()
+
+    def down_vote(self):
+        self.down_votes += 1
+        self.save()
